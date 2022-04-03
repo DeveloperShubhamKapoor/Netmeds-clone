45d80f7072da782109f65e76a5e0cc30e92a250c
Yogendrakokate <ykokate953@gmail.com>
2022-04-03 13:19:37 +0530

    Yogesh Changes

---
+++ Yogesh/sign.js
@@ -0,0 +1,10 @@
+/*signin.js file by yogendra*/
+
+document.querySelector("form").addEventListener("submit", myfunction);
+  
+signArr = [];
+function myfunction() {
+  event.preventDefault();
+
+  window.location.href = "signin.html";
+}
